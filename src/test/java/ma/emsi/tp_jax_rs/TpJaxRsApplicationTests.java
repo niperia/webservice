a4ad89d7e0b3e526b66fd103df45f5597c6cc785
niperia <haytamaatita2001@gmail.com>
2024-11-02 15:19:14 +0100

    frist commi

---
+++ src/test/java/ma/emsi/tp_jax_rs/TpJaxRsApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.tp_jax_rs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpJaxRsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
